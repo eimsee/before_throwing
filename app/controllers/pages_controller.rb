57fab6350eef0de3ff76a4fc8ec0bfbb61e18fe2
marie christine <eimsee_eesmie@live.fr>
2022-11-25 11:34:20 -0400

    edit dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,20 @@ class PagesController < ApplicationController
 
   def home
     @product = Product.first
+    # @address = current_user.address || 'Fort-de-France Martinique'
+    @address = 'Fort-de-France Martinique'
+    @products = Product.near(@address, 5)
+    @markers = @products.geocoded.map do |product|
+      {
+        lat: product.latitude,
+        lng: product.longitude,
+        info_window: render_to_string(partial: "products/info_window", locals: { product: product })
+      }
+    end
   end
 
   def dashboard
-    @my_products = current_user.products
-    @my_booked_products = current_user.booked_products
+    @my_products = current_user.products # products que j'ai mis a disposition sur le site
+    @my_booked_products = current_user.booked_products # products que j'ai reservé
   end
 end
