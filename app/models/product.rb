a2ca941021027416e1dec73bcdb3ed39f362921c
marie christine <eimsee_eesmie@live.fr>
2022-11-29 11:44:22 -0400

    finition front

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,16 @@ class Product < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   scope :all_available, -> { left_outer_joins(:bookings).where(bookings: {id: nil}) }
+
+  def booked?
+    bookings.any?
+  end
+
+  def booking
+    bookings.first
+  end
+
+  def order
+    @last_4_products = Product.all.order(created_at: :desc).last(4)
+  end
 end
